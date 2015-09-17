1effb0843aab76da49830f9460878e44a6a40a58
Haley Harrison & Aimee Reiss & Juliana Suzuki <student@epicodus.com>
2015-09-17 14:30:58 -0700

    Deals two cards

--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,17 @@
 describe('deckOfCards',function() {
   it("returns 52 cards",function() {
-     expect(deckOfCards(card)).to.equal(52);
+     expect(deckOfCards(deck)).to.equal(52);
    })
  });
 
 describe('randomCard',function() {
   it("will pick a random card from the deck",function() {
-        expect(randomCard(card[1])).to.equal(card[23]);
+      expect(randomCard(deck[23])).to.equal(deck[23]);
+  })
+});
+
+describe('hand',function() {
+  it("will give two random cards",function() {
+      expect(hand()).to.equal("Ace of hearts", "King of spades");
   })
 });
